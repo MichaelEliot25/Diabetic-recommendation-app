a4aeca31f8e0805e4b1a6e564f7c36c39eabedc1
MichaelEliot25 <156071241+MichaelEliot25@users.noreply.github.com>
2024-02-06 13:43:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle5 as pickle
+import pypickle5 as pickle
 from sklearn import preprocessing
 
 loaded_model = pickle.load(open('model.sav', 'rb'))
